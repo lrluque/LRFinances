bb44add19e53d0090824f6ed9f2db9aada5f32be
Lucas R. Luque <lucasrluque@icloud.com>
2021-12-26 18:22:04 +0100

    Add files via upload

---
+++ src/Application/Main.java
@@ -0,0 +1,27 @@
+package Application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.setTitle("LRFinances");
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setResizable(false);
+        Image icon = new Image("LRFinances.png");
+        primaryStage.getIcons().add(icon);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
